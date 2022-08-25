3c8639244061eacafd352ef4b8f85a792e67b923
Mahdi AlKhalaf <m@mahdi.pro>
2022-08-25 15:44:48 +0300

    feat: Add TypeORM and prepare for Heroku deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL || 'postgres://nest:nest@localhost:5432/nest_sample',
+      entities: [],
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
